bab34834e517a72aaafa62299dc72f1e722e066b
wenqinghua <wenqinghua@hnu.edu.cn>
2023-07-11 21:52:21 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,5 +1,10 @@
 package com.coolweather.android;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences preferences = getSharedPreferences("my_preferences", Context.MODE_PRIVATE);
+        String weatherString = preferences.getString("weather", null);
+        if (weatherString != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+//            Toast.makeText(getApplicationContext(), weatherString, Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+            finish();
+        }
     }
 }
